5cca22b0319062dd779d807e77cbe08c311c2b85
Anže Vodovnik <anze@vodovnik.com>
2015-10-03 22:25:36 +0200

    Trying various things around tracking events.

--- AppInsights/src/AppInsights.Web/Controllers/HomeController.cs
+++ AppInsights/src/AppInsights.Web/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.ApplicationInsights;
+using System.Text;
+using System.Reflection;
 
 namespace AppInsights.Web.Controllers
 {
@@ -26,18 +28,45 @@ namespace AppInsights.Web.Controllers
             ViewData["Message"] = "Your contact page.";
 
             var tc = new TelemetryClient();
-            tc.TrackEvent("Contact Initiated", new Dictionary<string, string>()
+            tc.TrackEvent("ContactInitiated", new Dictionary<string, string>()
             {
                 { "Name", "Test" },
-                { "Pricing plan", "A" }
-            });
+                { "PricingPlan", "A" }
+            });            
 
             return View();
+        }
+
+        public IActionResult GenerateEvents(int n = 100)
+        {
+            var tc = new TelemetryClient();
+
+            for (int i = 0; i < n; i++)
+            {
+                tc.TrackEvent("ActionPerformed");
+            }
+
+            tc.Flush();
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Error()
         {
             return View("~/Views/Shared/Error.cshtml");
+        }
+
+        public virtual ActionResult Status()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(Request.QueryString.ToString());
+
+            var version = typeof(HomeController).Assembly.GetName().Version;
+            
+
+            sb.AppendFormat("\r\nApplication version: {0}\r\n", version);
+            return Content(sb.ToString());
         }
     }
 }
